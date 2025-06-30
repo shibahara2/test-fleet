0425a847af6bcbf9fc36cdccbf069cdd79e03e73
Naoki Shibahara <shibahara@A1016620.local>
2025-06-30 11:16:50 +0900

    rename daemonset

--- test2.sh
+++
@@ -1,7 +0,0 @@
-ln -s /k3s /crictl
-echo "runtime-endpoint: unix:///run/k3s/containerd/containerd.sock" > crictl.yaml
-
-while true; do
-    ./crictl --config crictl.yaml images
-    sleep 600
-done
